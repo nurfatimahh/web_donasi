30c6b6496d8dcb661cca39bd3d87f3f6b47e833c
nurfatimahh <nurfatimahhh1603z@gmail.com>
2025-12-07 21:04:01 +0700

    first commit

---
+++ webdonasi/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DONASI</title>
+</head>
+<body>
+    <p>Majlis Ta'lim Riyadhul Badi'ah</p>
+</body>
+</html>
