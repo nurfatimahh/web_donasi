26252c9fd3f2cc711f46d51556a5a24514dcfa4d
smthlikeu <azkaferdianto225@gmail.com>
2025-12-13 12:14:42 +0700

    feat: membuat fitur halaman login

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,18 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return view('login');
-});
+// Route untuk login - middleware guest (hanya untuk yang belum login)
+Route::get('/login', [LoginController::class, 'showLoginForm'])->middleware('guest')->name('login');
+
+// Route logout - hanya untuk yang sudah login
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::get('/register', function () {
     return view('register');
