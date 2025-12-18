f50d10457d8224657928235f066ac676e954711c
nurfatimahh <nurfatimahhh1603z@gmail.com>
2025-12-18 17:39:46 +0700

    Pref: hahahahah

--- resources/views/admin/program/create.blade.php
+++ resources/views/admin/program/create.blade.php
@@ -26,7 +26,14 @@
                           class="w-full border rounded px-3 py-2 focus:outline-none focus:ring focus:ring-emerald-300"
                           placeholder="Deskripsi singkat program"></textarea>
             </div>
-
+            <div class="mb-4">
+                <label class="block mb-1 font-semibold text-gray-700">
+                    Target Donasi (Rp)
+                </label>
+                <input type="number"
+                       class="w-full border rounded px-3 py-2 focus:outline-none focus:ring focus:ring-emerald-300"
+                       placeholder="Contoh: 5000000">
+            </div>
             <div class="flex justify-between">
                 <a href="/admin/program"
                 class="px-4 py-2 rounded border border-gray-300 hover:bg-gray-100">
